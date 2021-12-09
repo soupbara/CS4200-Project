cb1f89955586164c8f7a01191cc4cffb19409ecf
Josephine <jmnguyen1999@gmail.com>
2021-12-09 15:00:26 -0800

    Adding new models: Answer and Attribute, and tons of helper methods for api calls

--- client.py
+++ client.py
@@ -2,10 +2,28 @@ import requests
 
 class ConceptNetClient:
 
-    BASE_URL = 'http://api.conceptnet.io/c/en/food'
+    BASE_URL = 'http://api.conceptnet.io'
+    # ---static Relation constants----
+    RELATION_LOCATION = "AtLocation"
+    RELATION_GET_TYPE = "IsA"
+    RELATION_PROPERTY = "HasProperty"
+    RELATION_USED_FOR = "UsedFor"
+    RELATION_MADE_OF = "MadeOf"
+    RELATION_CAN_BE = "ReceivesAction"
+    RELATION_CAPABLE = "CapableOf"
+
+    RELATE_TO_ENG = {RELATION_LOCATION:"Do you want something from ",
+                     RELATION_GET_TYPE: "Do you want ",
+                     RELATION_PROPERTY: "Do you want something that is ",
+                     RELATION_USED_FOR: "Do you want a food that is used for ",
+                     RELATION_MADE_OF: "Do you want something that is made of ",
+                     RELATION_CAN_BE: "Do you want something that can be ",
+                     RELATION_CAPABLE: "Do you want something that is able to "}
+
 
     def __init__(self):
-        self.jsonResponse = requests.get(self.BASE_URL).json()
+        pass
+        #self.jsonResponse = requests.get(self.BASE_URL).json()
 
     def printEdges(self):
         for i in range(len(self.jsonResponse['edges'])):
@@ -16,3 +34,150 @@ class ConceptNetClient:
 
     def printCurrJson(self):
         print(self.jsonResponse)
+
+    def generalFoodRequest(self):
+        return requests.get(self.BASE_URL+"/c/en/food").json()
+
+    def getFoodURI(self):
+        return "/c/en/food"
+
+    def makeRequest(self, queryString):
+        print(self.BASE_URL + queryString)
+        jsonResponse = requests.get(self.BASE_URL + queryString).json()
+
+        # if the id of jsonResponse isn't our queryString, our response is wrong
+        if jsonResponse.get("@id") != queryString:
+            print("Got the wrong response..")
+            return None
+        else:
+            return jsonResponse
+
+    def getQuestionFrame(self, code):
+        try:
+            return self.RELATE_TO_ENG.get(code)
+        except TypeError:
+             print("your code didn't match anything")
+             return None
+
+    def removeArticlesIn(self, word):
+        if "the " in word:
+            word = word.removeprefix("the ")
+        if "a " in word:
+            word = word.removeprefix("a ")
+        if "an " in word:
+            word = word.removeprefix("an ")
+
+        return word
+    #Purpose:      Get correct URI endpoint for the given word.
+    def getURI(self, word):
+        #queryString = "/uri?language=en&text="+word
+        #return requests.get(self.BASE_URL + queryString).json().get("@id")
+        word = self.removeArticlesIn(word)
+        if word.find(' ') != -1:
+            word = word.replace(' ', '_')
+            print("new edgeLabel: " + word)
+        return "/c/en/"+word
+
+    def getRelatednessVal(self, startUri, endUri):
+        return self.makeRequest("/relatedness?node1="+startUri+"&node2="+endUri)["value"]
+
+    #----------Relation queries-------------------------------:
+
+    #Purpose:     Get all possible relations between these two uris
+    def getRelationTypesBw(self, uri1, uri2):
+        return self.makeRequest("/query?node=" + uri1 + "&other=" + uri2)
+
+    #Purpose:     Get all properties of this uri
+    def getPropertiesOf(self, startUri):
+        return self.makeRequest("/query?start="+startUri+"&rel=/r/HasProperty")
+
+    #Properties:     Get all words that are types of this uri
+    def getTypesOf(self, endUri):
+        return self.makeRequest("/query?end=" + endUri + "&rel=/r/IsA")
+
+    #Purpose:   Get the specific edge if this startUri IS a type of this endUri
+    def confirmTypeOf(self, startUri, endUri):
+        return self.makeRequest("/query?start=" + startUri + "&end="+endUri + "&rel=/r/IsA").get("edges")
+
+    def getWhatMadeOf(self, startUri):
+        return self.makeRequest("/query?node="+startUri+"&rel=/r/MadeOf")
+
+    def getLocationOf(self, startUri):
+        return self.makeRequest(("/query?start="+startUri+"&rel=/r/AtLocation")).get("edges")
+
+    def getWhatIsLocatedAt(self, endUri, limitCount):
+        return requests.get(self.BASE_URL + "/query?end="+endUri+"&rel=/r/AtLocation&limit="+str(limitCount)).json().get("edges")
+        #return self.makeRequest(("/query?end="+endUri+"&rel=/r/AtLocation&limit="+str(limitCount))).get("edges")
+
+    def getWhatUsedFor(self, startUri):
+        return self.makeRequest(("/query?start=" + startUri + "&rel=/r/UsedFor")).get("edges")
+
+    #Purpose:   Returns the actual root word as a string. Just chooses the first result (highest weight) though, which may be buggy
+    def getRootWord(self, startUri):
+        response = self.makeRequest(("/query?start=" + startUri + "&rel=/r/FormOf")).get("edges")
+        if len(response) > 0:
+            return response[0]["end"]["label"]
+        return None
+
+    def getAllForms(self, startUri):
+        response = self.makeRequest(("/query?start=" + startUri + "&rel=/r/FormOf")).get("edges")
+        allForms = []
+        for node in response:
+            newForm = node["end"]["label"]
+            if not(newForm in allForms):
+                allForms.append(newForm)
+        print(str(allForms))
+        return allForms
+
+    #Purpose:   Return whether or not the given phrase is a food
+    def getIsFood(self, phrase):
+        if phrase != "food":
+            phraseUri = self.getURI(phrase)
+            # rootWord = self.getRootWord(self.getURI(phrase))
+            # if rootWord is not None:
+            #     phraseUri = self.getURI(rootWord)
+
+            # Case 1: Some words in ConceptNet don't register as "food" for some reason but do when you find the word sense_label:
+            case1 = self.makeRequest(phraseUri + "/n/wn/food").get("edges")
+            if len(case1) > 0:
+                return True
+
+            #Case 2: isA "food" returns an edge
+            case2 = self.confirmTypeOf(phraseUri, self.getFoodURI())
+            #If there is an edge & not actually the term "food", --> return true
+            if len(case2) > 0 and case2[0]["start"]["label"] != "food":
+                return True
+
+            #Case 3: if the typesOf this phrase are foods
+            typesOfPhrase = self.getTypesOf(phraseUri).get("edges")
+            counter = 1
+            for node in typesOfPhrase:
+                if counter < 2:         #let's say if the first 2 aren't foods, let's not bother checking the rest.
+                    isAFood = self.confirmTypeOf(self.getURI(node["start"]["label"]), self.getFoodURI())
+
+                    # If there is an edge & not actually the term "food", --> return true
+                    if len(isAFood) > 0 and isAFood[0]["start"]["label"] != "food":
+                        return True
+                    counter = counter + 1
+                else:
+                    break
+
+        #All else fails
+        return False
+
+
+
+
+#Interest:
+#- relatedness value between pairs of terms
+# - f(F)eature(s)
+# - possible relations: /query?node=/c/en/dog&other=/c/en/bark
+#- relations:
+# #   - properties of /r/HasProperty
+#     - location of   /r/AtLocation
+#     - is a type of:  /r/IsA
+#     - made of
+
+# Questions:
+# - breakfast food is a type of food (/query?start=/c/en/breakfast_food&end=/c/en/food&rel=/r/IsA)
+#     - what are the types of breakfast foods? (bc then they must also be foods) (query?start=/c/en/breakfast_food&rel=/r/IsA)
--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 #testing commit
 from client import ConceptNetClient
+from attribute import Attribute
+from answer import Answer
+
 import random
-import requests
 
 apiClient = ConceptNetClient()
-apiClient.printCurrJson()
-apiClient.printEdges()
+# print(apiClient.generalFoodRequest())
+# uri=apiClient.getURI("breakfast")
+# print(apiClient.getPropertiesOf(uri))
+# print(apiClient.getTypesOf(apiClient.getURI("one_unhealthy_food")))
+# print(apiClient.confirmTypeOf(apiClient.getURI("breakfast_food"), apiClient.getURI("food")))
+# print(apiClient.getWhatMadeOf(apiClient.getURI("chocolate")))
+# print(apiClient.getRelationTypesBw(apiClient.getURI("plate"), apiClient.getURI("food")))
+# print(apiClient.getRelatednessVal(apiClient.getURI("celebration"), apiClient.getFoodURI()))
+#print(apiClient.getRootWord(apiClient.getURI("pies")))
+
+possibleAnswers = {}    #track which Answers are still valid in choosing
+allAttributes = []      #track history of all positive Attributes
+nonpossibleAnswers = {}     #track which Answers we've already decided to throw out --> do NOT attempt to add it again!
+
+
 userPreferences = []
+
+MIN_POSS_ANSWERS = 30       #certain length of possibleAnswers[] necessary before able to create questions based on these probable answers.
+initializedAnswers = False
+MIN_PROBABILITY = 0.07
 
 #Purpose:  Given a list of valid options (ints), ensure user inputs one of the options. Used many time for random prompts.
 def getValidChoice(prompt, options):
@@ -33,44 +52,177 @@ def printMenu():
 
   return getValidChoice(str_menu, [1, 2])    #user must choose 1 or 2
 
-#Purpose: Use API to figure out what question to return next, format the question as a string and return it. Used by game()
-def findNextQuestion(currentQuestion):
- category = {"breakfast":"Are you looking for breakfast? (1) Yes, (2) No", "lunch":"Are you looking for lunch? (1) Yes, (2) No", "dinner":"Are you looking for dinner? (1) Yes, (2) No", "dessert":"Are you looking for desserts? (1) Yes, (2) No"}
- portion = {"full meal":"Do you want a full meal? (1) Yes, (2) No", "snack":"Do you want a snack? (1) Yes, (2) No", "finger food":"Would you like finger food? (1) Yes, (2) No", "ready-to-eat":"Would you like ready-to-eat food? (1) Yes, (2) No"}
- type = {"beverage":"Would you like a beverage? (1) Yes, (2) No", "solid food":"Would you like solid food? (1) Yes, (2) No", "semi-solid food":"Would you like semi-solid food? (1) Yes, (2) No"}
- flavor = {"sweet":"Would you like something sweet? (1) Yes, (2) No", "savory":"Would you like something savory? (1) Yes, (2) No", "sour":"Would you like something sour? (1) Yes, (2) No", "spicy":"Would you like spicy food? (1) Yes, (2) No", "bitter":"Would you like something bitter? (1) Yes, (2) No"}
- restriction = {"vegan":"Do you want something vegan? (1) Yes, (2) No", "meat":"Are you able to eat meat? (1) Yes, (2) No", "gluten":"Are you able to eat gluten? (1) Yes, (2) No", "nuts":"Are you able to eat nuts? (1) Yes, (2) No", "dairy":"Are you able to eat dairy products? (1) Yes, (2) No"}
+#Purpose:   Return which Answer is the most probable (highest probability) right now, used in findNextQuestion()
+def findMostProbableAnswer():
+    for answer in possibleAnswers:
+        pass
 
- #Randomly select a question to ask, however it may repeat if the user continuously answers 'no'
- if currentQuestion == 1:
-     return random.choice(list(category.items()))
- if currentQuestion == 2:
-     return random.choice(list(portion.items()))
- if currentQuestion == 3:
-     return random.choice(list(type.items()))
- if currentQuestion == 4:
-     return random.choice(list(flavor.items()))
- else:
-     return random.choice(list(restriction.items()))
+#Purpose:   Return the highest relatedness value from api for this phrase. Relatedness value based on this phrase and "food"
+def getOverallRelatedness(phrase):
+    phraseArr = phrase.split()
+    max = -5        #assume no relatedness value can ever be below -5
+    for word in phraseArr:
+        currVal = apiClient.getRelatednessVal(apiClient.getURI(word), apiClient.getFoodURI())
+        if currVal > max and word != "food":
+            max = currVal
+    return max
+
+#Purpose: Use API to figure out what question to return next, format the question as a string and return it. Used by game()
+def findNextQuestion():
+    global initializedAnswers, MIN_POSS_ANSWERS, possibleAnswers, allAttributes, nonpossibleAnswers
+
+    #1.) Do we have enough in our possibleAttributes to choose randomly, or choose only on general food?
+    if initializedAnswers:
+        #Randomly choose how we get the next question: (1) Choose question based on possible answer, (2) Choose question based on general foods (expands the possibleAnswer[])
+
+        pass
+    else:
+        #Choose either to base next question on location or, choose some interval of types of food on possibleAnswers and call this method again. (purpose of this: expands the possibleAnswer[])
+        choice = random.randint(0, 1)
+
+        #do question based on locationAt relation on general food node
+        if choice == 0:
+            edges = apiClient.getLocationOf(apiClient.getFoodURI())
+
+            askedQuestion = False
+            while not askedQuestion:
+                #Choose random edge, determine if its relatedness value to "food" is high enough, --> ask question about it if so:
+                rIndex = random.randint(0, len(edges)-1)
+                print(rIndex)
+
+                edgeLabel = edges[rIndex]["end"]["label"]
+                print(edgeLabel)
+
+                if getOverallRelatedness(edgeLabel) > 0.1:
+                    askedQuestion = True
+
+                    #ask the question:
+                    question = apiClient.getQuestionFrame(apiClient.RELATION_LOCATION)+edgeLabel+"? Enter: (1) Yes or (2) No"
+                    userInput = getValidChoice(question, [1, 2])
+
+                    #If said yes --> (1) create a new Attribute to track, (2) find all possible answers that satify this new Attribute + save into possibleAnswers
+                    if userInput == 1:
+                        newAttribute = Attribute(apiClient.RELATION_LOCATION, edgeLabel)
+                        allAttributes.append(newAttribute)
+
+                        #Find all possibleAnswers: all nodes that IsA food & is AtLocation edgeLabel:
+                        #1.) Get things located at:
+                        response = apiClient.getWhatIsLocatedAt(apiClient.getURI(edgeLabel), 20)
+
+                        #2.) For each edge, save only the ones that are also a food:
+                        validPossAnswers = []
+                        for node in response:
+                            #Get the label, ensure we are using the root most form of the word
+                            label = node["start"]["label"]
+                            print("Before getting root:" + label)
+                            label = apiClient.removeArticlesIn(label)
+                            rootWord = apiClient.getRootWord(apiClient.getURI(label))
+                            if rootWord is not None:
+                                label = rootWord               #only ever stores ROOT words, --> easier to check, don't have to worry about any different forms
+                            print('after getting root and removing articles: '+label)
+                            #If label IS a food, and NOT yet in possibleAnswers, store into validPossAnswers to incorporate into possibleAnswers later:
+                            if apiClient.getIsFood(label):
+                                validPossAnswers.append(label)
+
+
+                                # if not (label in possibleAnswers.keys()):
+                                #     possibleAnswers[label] = Answer(newAttribute, label)
+                                #
+                                # #If already seen this word --> increase probability, add this newAttribute it qualifies for
+                                # else:
+                                #     answer = Answer(possibleAnswers.get(label))
+                                #     answer.addProbability()
+                                #     answer.addAttribute(newAttribute)
+
+                        #3.) Now we know what should be inside possibleAnswers bc validPossAnswers is populated
+                        #           --> iterate through possibleAnswers and update ALL probabilities of ALL possible answers based on what has and hasn't been seen this iteration
+                        for key in possibleAnswers.keys():
+                            answer = Answer(possibleAnswers.get(key))
+                            #if we already have the answer in possibleAnswers --> increase probability, add this newAttribute it qualifies for
+                            if key in validPossAnswers:
+                                answer.addProbability()
+                                answer.addAttribute(newAttribute)
+
+                                #remove from validPossAnswers:
+                                validPossAnswers.remove(key)
+
+                            #We didn't see this answer this iteration --> decrease probability
+                            else:
+                                answer.subProbability()
+                                #is the probability too low now? yes --> remove it and no longer consider it for the future purposes
+                                if answer.getProbability() < MIN_PROBABILITY:
+                                    print("possAnswers length before pop: " + str(len(possibleAnswers)))
+                                    possibleAnswers.pop(key)
+                                    print("possAnswers length after pop: " + str(len(possibleAnswers)))
+                                    nonpossibleAnswers[key] = answer
+
+                        #4.) At this point, validPossAnswers should only be filled with Answer labels that are NOT yet in possibleAnswers{} --> can add it now:
+                        for answerLabel in validPossAnswers:
+                            if not(answerLabel in nonpossibleAnswers.keys()):
+                                possibleAnswers[answerLabel] = Answer(newAttribute, answerLabel)
+
+
+                        #for testing reasons:
+                        print("possAnswers:  ")
+                        for answer in possibleAnswers.items():
+                            print(answer[1].getLabel())
+
+
+
+        #do NOT choose a question, instead, pick a random range and populate random interval of isA relation into possibleAnswers, + call method again
+        else:
+            edges = apiClient.getTypesOf(apiClient.getFoodURI())
+
+            #Choose random sample of of random size of edges to look at
+            section = random.sample(list(edges), random.randint(1, 20 if len(edges) > 20 else len(edges)))
+
+
+        if len(possibleAnswers) >= MIN_POSS_ANSWERS:
+            initializedAnswers = True
+
+
+findNextQuestion()
+
+
+ # category = {"breakfast":"Are you looking for breakfast? (1) Yes, (2) No", "lunch":"Are you looking for lunch? (1) Yes, (2) No", "dinner":"Are you looking for dinner? (1) Yes, (2) No", "dessert":"Are you looking for desserts? (1) Yes, (2) No"}
+ # portion = {"full meal":"Do you want a full meal? (1) Yes, (2) No", "snack":"Do you want a snack? (1) Yes, (2) No", "finger food":"Would you like finger food? (1) Yes, (2) No", "ready-to-eat":"Would you like ready-to-eat food? (1) Yes, (2) No"}
+ # type = {"beverage":"Would you like a beverage? (1) Yes, (2) No", "solid food":"Would you like solid food? (1) Yes, (2) No", "semi-solid food":"Would you like semi-solid food? (1) Yes, (2) No"}
+ # flavor = {"sweet":"Would you like something sweet? (1) Yes, (2) No", "savory":"Would you like something savory? (1) Yes, (2) No", "sour":"Would you like something sour? (1) Yes, (2) No", "spicy":"Would you like spicy food? (1) Yes, (2) No", "bitter":"Would you like something bitter? (1) Yes, (2) No"}
+ # restriction = {"vegan":"Do you want something vegan? (1) Yes, (2) No", "meat":"Are you able to eat meat? (1) Yes, (2) No", "gluten":"Are you able to eat gluten? (1) Yes, (2) No", "nuts":"Are you able to eat nuts? (1) Yes, (2) No", "dairy":"Are you able to eat dairy products? (1) Yes, (2) No"}
+ #
+ # #Randomly select a question to ask, however it may repeat if the user continuously answers 'no'
+ # if currentQuestion == 1:
+ #     return random.choice(list(category.items()))
+ # if currentQuestion == 2:
+ #     return random.choice(list(portion.items()))
+ # if currentQuestion == 3:
+ #     return random.choice(list(type.items()))
+ # if currentQuestion == 4:
+ #     return random.choice(list(flavor.items()))
+ # else:
+ #     return random.choice(list(restriction.items()))
 
 
 #Purpose: Use API to determine if we found a good enough answer! E.g. is there anything else we could ask, how confident are we in this answer, etc.
 def gameFinished():
- path = 'http://api.conceptnet.io/c/en/' + userPreferences[0] #Add the user's data to the path (first item in userPreferences)
- obj = requests.get(path).json() #Get method to retrieve the data
- index = 0
+    pass
+ # path = 'http://api.conceptnet.io/c/en/' + userPreferences[0] #Add the user's data to the path (first item in userPreferences)
+ # obj = requests.get(path).json() #Get method to retrieve the data
+ # index = 0
+ #
+ # #Print out results from ConceptNet
+ # for i in range(len(obj['edges'])):
+ #     print(obj['edges'][i]['end']['label'])
+ #     #if '/r/IsA' in obj['edges'][i]['end']['label']:
+ #         #index = i
+ #     for key in ['rel', 'surfaceText']:
+ #         print(obj['edges'][i][key])
+ #
+ # #Print suggested item to user
+ # print("Answer generated! You might enjoy: " + str(obj['edges'][index][key]))
+ # print("---------------------------------------\n")
 
- #Print out results from ConceptNet
- for i in range(len(obj['edges'])):
-     print(obj['edges'][i]['end']['label'])
-     #if '/r/IsA' in obj['edges'][i]['end']['label']:
-         #index = i
-     for key in ['rel', 'surfaceText']:
-         print(obj['edges'][i][key])
 
- #Print suggested item to user
- print("Answer generated! You might enjoy: " + str(obj['edges'][index][key]))
- print("---------------------------------------\n")
 
 
 #Purpose:  Runs one whole game of Crave! Uses findNextQuestion(), gameFinished(), and get
@@ -135,4 +287,4 @@ def main():
 
 
 
-main()
+#main()
